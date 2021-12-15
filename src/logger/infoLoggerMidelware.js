3459748a747c94559e60faf5c32b56ceb9b06a1f
Yurii Petrashenko <yurii.petrashenko@wickes.co.uk>
2021-12-15 21:45:40 +0200

    [homework-3] Add methods execution time logging

--- src/logger/infoLoggerMidelware.js
+++ src/logger/infoLoggerMidelware.js
@@ -1,14 +1,16 @@
 import { methodInvocationLogger as logger }  from './logger'
 
-const LOG_MESSAGE_TEMPLATE = 'Method: %s, Status: %s, URL: %s, Body: %s, Params: %s, Query params: %s'
+const LOG_MESSAGE_TEMPLATE = 'Method: %s, Status: %s, URL: %s, Body: %s, Params: %s, Query params: %s, Execution time: %sms'
 
 function logMethodInvocation (req, res, next) {
+  const startTime = Date.now();
   res.on('finish', () => {
     const { method, originalUrl, body, params, query } = req
     const { statusCode } = res
 
+    const duration = Date.now() - startTime
     logger.debug(LOG_MESSAGE_TEMPLATE, method, statusCode, originalUrl,
-      JSON.stringify(body), JSON.stringify(params), JSON.stringify(query))
+      JSON.stringify(body), JSON.stringify(params), JSON.stringify(query), duration)
   })
 
   next()
