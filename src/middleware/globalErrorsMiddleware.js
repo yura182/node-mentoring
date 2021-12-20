9bf238eed47af71497d9e1254dfc6cec5392c214
Yurii Petrashenko <yurii.petrashenko@wickes.co.uk>
2021-12-20 21:33:43 +0200

    [homework-3] Add eslint config to ignore unused args with specific name pattern

--- src/middleware/globalErrorsMiddleware.js
+++ src/middleware/globalErrorsMiddleware.js
@@ -4,7 +4,7 @@ import { INTERNAL_SERVER_ERROR } from '../error/errorMessages'
 
 const ERROR_MESSAGE = 'Error occurred.'
 
-function handleError (err, req, res, next) {
+function handleError (err, req, res, _) {
   logger.error(ERROR_MESSAGE, err)
   res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ errorMessage: INTERNAL_SERVER_ERROR })
 }
